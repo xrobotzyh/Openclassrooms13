14aa5c20e95825a7e342c1f86593a323fb354ae8
zhao yh <makai87kai@outlook.com>
2023-11-10 13:27:57 +0100

    add unite test to lettings and profiles

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,34 @@
 # Create your tests here.
-def test_dummy():
-    assert 1
+from django.test import TestCase
+
+from .models import Letting, Address
+
+
+class MyTestCase(TestCase):
+
+    def setUp(self):
+        Address.objects.create(
+            number=1,
+            street='generale leclerc',
+            city='paris',
+            state='75',
+            zip_code='75001',
+            country_iso_code='fr'
+        )
+        Letting.objects.create(title='test', address_id=1)
+
+    def tearDown(self):
+        Address.objects.all().delete()
+        Letting.objects.all().delete()
+
+    def test_letting_index_can_be_opened(self):
+        response = self.client.get('/lettings/')
+        assert response.status_code == 200
+
+    def test_letting_object_can_be_opened(self):
+        response = self.client.get('/lettings/1/')
+        assert response.status_code == 200
+
+    # def test_none_letting_object_can_be_opend(self):
+    #     response = self.client.get('/lettings/17/')
+    #     assert response.status_code == 404
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,21 @@
+from django.test import TestCase
+from .models import Profile
 
 
-# Create your tests here.
+class MyTestCase(TestCase):
+
+    def setUp(self):
+        Profile.objects.create(favorite_city='paris', user_id=1)
+
+    def tearDown(self):
+        Profile.objects.all().delete()
+
+    def test_profile_index_page_can_be_opened(self):
+        response = self.client.get('/profiles/')
+        assert response.status_code == 200
+
+    # def test_letting_object_page_can_be_opened(self):
+    #     response = self.client.get('/profiles/2/')
+    #     assert response.status_code == 200
+
+
