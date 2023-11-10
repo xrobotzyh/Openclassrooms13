a9edba7151e1c232d9b96e13f7a77a33ebeca516
zhao yh <makai87kai@outlook.com>
2023-11-10 10:01:36 +0100

    flake8 passed

--- lettings/views.py
+++ lettings/views.py
@@ -1,5 +1,3 @@
-import os
-
 from django.shortcuts import render
 
 from lettings.models import Letting
--- profiles/views.py
+++ profiles/views.py
@@ -18,6 +18,3 @@ def profile(request, username):
     profile = Profile.objects.get(user__username=username)
     context = {'profile': profile}
     return render(request, 'profiles/profile.html', context)
-
-
-
