0f7ee617961971241a11d36c33d7970fca01dc07
zhao yh <makai87kai@outlook.com>
2023-11-15 20:57:44 +0100

    delete redundant code

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,7 +3,6 @@ import os
 from pathlib import Path
 
 import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
 from dotenv import DotEnv
 
 
@@ -126,7 +125,6 @@ SENTRY_DSN = dotenv.get('SENTRY_DSN')
 
 sentry_sdk.init(
     dsn=SENTRY_DSN,
-    integrations=[DjangoIntegration()],
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
