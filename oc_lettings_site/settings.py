9d76297bd5058d82f68c9da4c055400897cfa15c
zhao yh <makai87kai@outlook.com>
2023-12-06 21:48:04 +0100

    update timezone postresql

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -81,9 +81,6 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD', 'password'),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
         'PORT': os.environ.get('DB_PORT', '5432'),
-        'OPTIONS': {
-            'timezone': 'UTC',
-        },
     }
 }
 
@@ -116,7 +113,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
