8c9914ee94a0e19ccbe7699fd30804ea4cafaab5
zhao yh <makai87kai@outlook.com>
2023-12-06 22:56:54 +0100

    update timezone postresql

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$RUN_MIGRATIONS" = "true" ]; then
-    render run python manage.py migrate
+  render run python manage.py migrate
 fi
 
 python manage.py runserver 0.0.0.0:$PORT
