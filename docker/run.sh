b6f1446a5ce4c6baf68014ceb8f091143c231b97
zhao yh <makai87kai@outlook.com>
2023-12-07 09:35:01 +0100

    copy data

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,6 @@
 #  render run python manage.py migrate
 #fi
 python manage.py migrate
-python manage.py dumpdata > data.json
-python manage.py loaddata data.json
+#python manage.py dumpdata > data.json
+#python manage.py loaddata data.json
 python manage.py runserver 0.0.0.0:$PORT
