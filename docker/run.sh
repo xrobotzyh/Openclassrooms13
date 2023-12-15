cc3f28f8c0e414439152a98f51bfc4afedb2dc14
zhao yuhao <makai87kaius@hotmail.com>
2023-12-15 13:35:36 +0100

    update readme

--- docker/run.sh
+++ docker/run.sh
@@ -2,10 +2,8 @@
 
 #if [ "$RUN_MIGRATIONS" = "true" ]; then
 #  render run python manage.py migrate
-#fi
+#fi##
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py dumpdata > data.json
-python manage.py loaddata data.json
 python manage.py runserver 0.0.0.0:$PORT
