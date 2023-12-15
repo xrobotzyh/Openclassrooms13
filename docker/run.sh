15272b33bc9d1472b5a5a77bdea9b9ed2ee93c3a
zhao yuhao <makai87kaius@hotmail.com>
2023-12-15 14:10:48 +0100

    update readme

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-#if [ "$RUN_MIGRATIONS" = "true" ]; then
-#  render run python manage.py migrate
-#fi##
+if [ "$RUN_MIGRATIONS" = "true" ]; then
+  python manage.py migrate
+fi
 
-python manage.py migrate
-python manage.py collectstatic --noinput
+if [ "$RUN_COLLECTSTATIC" = "true" ]; then
+  python manage.py collectstatic --noinput
+fi
 python manage.py runserver 0.0.0.0:$PORT
