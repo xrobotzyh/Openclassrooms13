5578279c55db2263dd66eed43d1b98add347442a
zhao yuhao <makai87kaius@hotmail.com>
2023-12-16 23:00:31 +0100

    update readme

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,6 +118,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR / "static", ]
+
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
