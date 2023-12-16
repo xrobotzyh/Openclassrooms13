4f64f3c86a2ebca1a157fed7ca4c1da45d8b885f
zhao yuhao <makai87kaius@hotmail.com>
2023-12-16 23:10:54 +0100

    update readme

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [BASE_DIR / "static", ]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
