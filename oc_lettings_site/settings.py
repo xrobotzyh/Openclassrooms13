6a9aaf7b7a71eb7c7873300f4c8c04fb311e47a2
zhao yuhao <makai87kaius@hotmail.com>
2023-12-16 22:56:20 +0100

    update readme

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -118,8 +118,9 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static", ]
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('sentry-debug/', trigger_error),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 handler404 = page_404
 handler500 = page_500
